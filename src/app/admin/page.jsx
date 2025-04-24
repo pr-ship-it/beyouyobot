270ac29ecd3f005c3c3a2fad58df05c6f7e1b0a6
pr-ship-it <developer6969.tech@gmail.com>
2025-04-24 11:52:09 -0500

    Update page.jsx

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import "../../../styles/admin.css"
+import "../../styles/admin.css"
 export default function Login() {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
