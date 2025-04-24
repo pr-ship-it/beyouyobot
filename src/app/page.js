b8827df7d72eb9639aa7a806e3536eff1a15ee03
pr-ship-it <developer6969.tech@gmail.com>
2025-04-24 11:51:06 -0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 'use client';
-
+import "../styles/admin.css"
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
@@ -11,10 +11,7 @@ export default function Home() {
 
   return (
     <div className="home">
-      <h1 className="home__title">Bot de Cotización</h1>
-      <p className="home__description">
-        Este es el panel de control para el bot de Telegram de cotización de USDT/MXN.
-      </p>
+
       <button className="home__button" onClick={handleAdminRedirect}>
         Ir al Panel de Administración
       </button>
